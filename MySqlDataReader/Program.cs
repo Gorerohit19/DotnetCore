0b2141c7e0e623040f3cea6ecfbc3e023f3a8b0c
unknown <gore.rohit1902@gmail.com>
2022-12-06 22:05:38 +0530

    MySqlDataReader Program File

---
+++ MySqlDataReader/Program.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+
+string conStr = "server=localhost; uid=root; password=Rohit@7378;database=classicmodels";
+
+MySqlConnection con = new MySqlConnection();
+
+try{
+    con.ConnectionString=conStr;
+    con.Open();
+
+    string sql ="Select customerName, country from customers where country='USA' ";
+    MySqlCommand cmd = new MySqlCommand(sql, con);
+    MySqlDataReader rdr = cmd.ExecuteReader();
+
+    while(rdr.Read()){
+        Console.WriteLine(rdr[0]+"--"+rdr[1]);
+    }
+    rdr.Close();
+
+
+}
+catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+finally{
+con.Close();
+}

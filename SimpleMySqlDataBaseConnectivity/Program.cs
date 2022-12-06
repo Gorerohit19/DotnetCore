9a6253f3eaa1ce5a6615519a7e5d431f66367287
unknown <gore.rohit1902@gmail.com>
2022-12-06 15:28:26 +0530

    Mysql Connectivity

---
+++ SimpleMySqlDataBaseConnectivity/Program.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+
+string conStr = "server=localhost; uid=root; password=;database=classicmodels";
+
+MySqlConnection con = new MySqlConnection();
+
+try {
+    con.ConnectionString=conStr;
+    con.Open();
+    string query = "select * from employees";
+    MySqlCommand cmd = new MySqlCommand(query, con);
+    MySqlDataReader rdr = cmd.ExecuteReader();
+
+    while(rdr.Read()){
+        Console.WriteLine(rdr[0]+":"+rdr[1]+":"+rdr[2]+":"+rdr[4]);
+        Console.WriteLine(rdr["employeeNumber"]+":"+rdr["lastName"]+":"+rdr["firstName"]+":"+rdr["email"]);
+    }
+    rdr.Close();
+
+}
+catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+finally{
+ con.Close();
+}

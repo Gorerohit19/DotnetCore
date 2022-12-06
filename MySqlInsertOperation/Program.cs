7cd63db5b4017fa538a13855f55c225d8828ba7f
unknown <gore.rohit1902@gmail.com>
2022-12-06 22:55:15 +0530

    MySql Insert Operation

---
+++ MySqlInsertOperation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+string conStr = "server=localhost; uid=root; password=Rohit@7378; database=demo1";
+MySqlConnection con = new MySqlConnection(conStr);
+
+try{
+    con.Open();
+    string query = "insert into employee (EmpId, EmpName, EmpAge, EmpDept) values(5,'Rohan', 20, 'Sales')";
+    MySqlCommand cmd = new MySqlCommand(query, con);
+    cmd.ExecuteNonQuery();
+
+}
+catch(Exception e){
+    Console.WriteLine(e.Message);
+}
+finally{
+    con.Close();
+}
+

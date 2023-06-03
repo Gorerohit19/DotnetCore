aa6485b084e87902cd2b8ebdc35d9af4426272c5
unknown <gore.rohit1902@gmail.com>
2023-06-03 23:53:13 +0530

    FileIo Operations

---
+++ FileIOApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Collections.Generic;
+
+string filePath = @"F:\FilePath\Test.txt";
+
+if(File.Exists(filePath))
+{
+    File.Delete(filePath);
+}
+
+//Exception Handling
+try{
+    //Text Write
+    using(TextWriter writer = File.CreateText(filePath))
+    {
+        string data ="My name is Rohit Gore";
+        writer.WriteLine(data);
+    }
+
+    // Text Reading 
+    using(TextReader reader = File.OpenText(filePath))
+    {
+        string data = reader.ReadToEnd();
+        Console.WriteLine(data);
+    }
+}
+    catch(Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
---
+++ FileIOBinaryApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text;
+
+string filePath=@"F:\FilePath\Binary.txt";
+
+
+
+// If you want to delete Existing File
+if(File.Exists(filePath))
+{
+    File.Delete(filePath);
+}
+
+
+// Creating new File (Binary) and Writing Data inside
+
+using(FileStream fs = File.Create(filePath)){
+    string data="This is Transflower data";
+    UTF8Encoding endcoder = new UTF8Encoding(true);
+
+    //string data is Converted into Bytes
+    byte[] binaryData = endcoder.GetBytes(data);
+
+    // Binary data is Written into file
+    fs.Write(binaryData, 0,binaryData.Length);
+}
+
+//Now We would try code for reading data from binary file
+//get STream in read mode
+
+using(FileStream fs = File.OpenRead(filePath))
+{
+    byte[] binaryData = new byte[1024];
+    UTF8Encoding encoder = new UTF8Encoding(true);
+
+    while(fs.Read(binaryData, 0, binaryData.Length)>0){
+        //Data Which is in Binary format is COnverted into string
+        string dataFromFile = encoder.GetString(binaryData);
+        Console.WriteLine(dataFromFile);
+    }
+}

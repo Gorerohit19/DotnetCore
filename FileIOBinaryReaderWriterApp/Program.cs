fb21b63745cad34d7ba88ad51ea9cfedcadeb7c5
unknown <gore.rohit1902@gmail.com>
2023-06-03 23:58:50 +0530

    File IO Operations

---
+++ FileIOBinaryReaderWriterApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+string filePath=@"F:\FilePath\BinaryWriter.txt";
+
+//Write data into file using BinaryWriter class
+
+FileStream fs=new FileStream(filePath, FileMode.Create);
+using(BinaryWriter bw=new BinaryWriter(fs)){
+    bw.Write(19.25);
+    bw.Write("Welcome to Transflower for learning technolgy ");
+    bw.Write(300);
+    bw.Write(true);
+}
+
+//Read data from file using BinaryReader
+
+FileStream fs2=File.Open(filePath, FileMode.Open);
+
+using(BinaryReader br=new BinaryReader(fs2)){
+    Console.WriteLine(br.ReadDouble());
+    Console.WriteLine(br.ReadString());
+    Console.WriteLine(br.ReadInt32());
+    Console.WriteLine(br.ReadBoolean());
+}
---
+++ FileIOStreamReaderWriteApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System.IO;
+
+string filePath = @"F:\FilePath\StreamWriter.txt";
+
+//File content Writing
+FileStream fs = new FileStream(filePath, FileMode.Create);
+using (StreamWriter sw = new StreamWriter(fs)){
+    sw.WriteLine(" Welcome To Transflower");
+    sw.WriteLine("\n Learning Experience");
+    sw.WriteLine("\n Doing ordinary things extra ordinary");
+}
+
+// Reading File Content
+
+using(StreamReader sr = new StreamReader(filePath)){
+    string txt;
+    //Reading data from File, Until the end of File is Reached 
+    while((txt=sr.ReadLine())!=null){
+        Console.WriteLine(txt);
+    }
+}
